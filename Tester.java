b9d188171e5503ccaf47ce279012315a8b9f52a3
superbrothers8 <bwood1178@gmail.com>
2022-11-13 09:00:00 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,8 +8,30 @@ public class Tester {
 	public static void main(String[] args) throws IOException {
 		File database = new File("src/Car Database.txt");
 		Scanner scan = new Scanner(database);
-//		FIND A WAY TO EFFECTIVELY WRITE TO FILES
+		FileWriter fw = new FileWriter(database, true);
+		CarInventory inv = new CarInventory();
+//		fw.write(inv.toString());
+		while (scan.hasNext()) {
+//			createCar(String price, String mileage, String make, String model, String mpg, String drive, String engine, String automatic)
+			if (scan.nextLine().equals("----------CAR----------")) {
+				String price = scan.nextLine();
+				String miles = scan.nextLine();
+				String make = scan.nextLine();
+				String model = scan.nextLine();
+				String mpg = scan.nextLine();
+				String drive = scan.nextLine();
+				String engine = scan.nextLine();
+				String automatic = scan.nextLine();
+				int carNum = Integer.parseInt(scan.nextLine());
+				inv.createCar(price, miles, make, model, mpg, drive, engine, automatic);
+			} else {
+				break;
+			}
+		}
+		
+		System.out.println(inv.toString());
 		scan.close();
+		fw.close();
 	}
 
 }

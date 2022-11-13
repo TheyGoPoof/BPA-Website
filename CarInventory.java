5f3f09d795af5c075a8cd3ef9f77f820d548d303
superbrothers8 <bwood1178@gmail.com>
2022-11-13 08:58:47 -0500

    Update CarInventory.java

--- CarInventory.java
+++ CarInventory.java
@@ -1,53 +1,90 @@
+import java.util.ArrayList;
 
 public class CarInventory {
-	private boolean previouslyOwned;
-	private int price;
-	private String color;
-	private String make;
-	private String model;
-	private int range;
-	private String[] driveOptions = {"FWD", "RWD", "AWD", "4WD"};
-	private String drive;
-	private String engine;
-	private boolean automatic;
+	private ArrayList<String[]> cars = new ArrayList<>();
 	
-	public CarInventory(boolean owned, int p, String c, String ma, String mo, int r, int d, String e, boolean a) {
-		previouslyOwned = owned;
-		price = p;
-		color = c;
-		make = ma;
-		model = mo;
-		range = r;
-		drive = driveOptions[d];
-		engine = e;
-		automatic = a;
+	public CarInventory() {
+		
+	}
+
+	public void createCar(String p, String mi, String ma, String mo, String m, String d, String e, String a) {
+		String price = p;
+		String miles = mi;
+		String make = ma;
+		String model = mo;
+		String mpg = m;
+		String drive = d;
+		String engine = e;
+		String automatic = a;
+		String[] newCar = {price, miles, make, model, mpg, drive, engine, automatic};
+		cars.add(newCar);
 	}
 	
-	public void setPreviouslyOwned(boolean owned) {
-		previouslyOwned = owned;
+	public String toString() {
+		String Car = "";
+		for (int i = 0; i < cars.size(); i++) {
+			Car += "\n" + "----------CAR----------";
+			Car += "\n" + cars.get(i)[0];
+			Car += "\n" + cars.get(i)[1];
+			Car += "\n" + cars.get(i)[2];
+			Car += "\n" + cars.get(i)[3];
+			Car += "\n" + cars.get(i)[4];
+			Car += "\n" + cars.get(i)[5];
+			Car += "\n" + cars.get(i)[6];
+			Car += "\n" + cars.get(i)[7];
+			Car += "\n" + i;
+		}
+		return Car;
 	}
-	public void setPrice(int p) {
-		price = p;
+	public void setPrice(String p, int n) {
+		cars.get(n)[0] = p;
 	}
-	public void setColor(String c) {
-		color = c;
+	public void setMiles(String mi, int n) {
+		cars.get(n)[1] = mi;
 	}
-	public void setPreviouslyOwned(String ma) {
-		make = ma;
+	public void setMake(String ma, int n) {
+		cars.get(n)[2] = ma;
 	}
-	public void setModel(String mo) {
-		model = mo;
+	public void setModel(String mo, int n) {
+		cars.get(n)[3] = mo;
 	}
-	public void setRange(int r) {
-		range = r;
+	public void setMpg(String m, int n) {
+		cars.get(n)[4] = m;
 	}
-	public void setDrive(int d) {
-		drive = driveOptions[d];
+	public void setDrive(String d, int n) {
+		cars.get(n)[5] = d;
 	}
-	public void setEngine(String e) {
-		engine = e;
+	public void setEngine(String e, int n) {
+		cars.get(n)[6] = e;
 	}
-	public void setAutomatic(boolean a) {
-		automatic = a;
+	public void setAutomatic(String a, int n) {
+		cars.get(n)[7] = a;
+	}
+	public String getPrice(int n) {
+		return cars.get(n)[0];
+	}
+	public String getMiles(int n) {
+		return cars.get(n)[1];
+	}
+	public String getMake(int n) {
+		return cars.get(n)[2];
+	}
+	public String getModel(int n) {
+		return cars.get(n)[3];
+	}
+	public String getMpg(int n) {
+		return cars.get(n)[4];
+	}
+	public String getDrive(int n) {
+		return cars.get(n)[5];
+	}
+	public String getEngine(int n) {
+		return cars.get(n)[6];
+	}
+	public String getAutomatic(int n) {
+		return cars.get(n)[7];
+	}
+	public String[] getCar(int n) {
+		return cars.get(n);
 	}
 }
